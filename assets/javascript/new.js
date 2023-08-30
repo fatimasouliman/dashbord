8d4df4e492c1bdfb9a14ab747ffbbd4c856e0584
fatimasouliman <fffaat.sss@gmail.com>
2023-08-30 23:17:41 +0300

    edit

--- assets/javascript/new.js
+++ assets/javascript/new.js
@@ -32,3 +32,25 @@ inputDate.valueAsDate = new Date();
 // addImg.addEventListener('click', () => {
 //     coverLabel.style = 'display:none'
 // })
+const labels = document.querySelectorAll(".title-lable");
+const inputs = document.querySelectorAll(".input-title");
+for (let i = 0 ; i < inputs.length ; i++) {
+   inputs[i].onfocus = () => {
+      labels[i].classList.add("active-label");
+   }
+   inputs[i].onblur = () => {
+      if(inputs[i].value === "")
+      labels[i].classList.remove("active-label")
+   }
+}
+const labelsTags = document.querySelectorAll(".label-tags");
+const inputsTags = document.querySelectorAll(".tags");
+for (let i = 0 ; i < inputsTags.length ; i++) {
+   inputsTags[i].onfocus = () => {
+      labelsTags[i].classList.add("active-label");
+   }
+   inputsTags[i].onblur = () => {
+      if(inputs[i].value === "")
+      labelsTags[i].classList.remove("active-label")
+   }
+}
